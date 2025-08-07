b4828b8001acd84428ba2374417c62dcc549a05d
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-08 01:29:45 +0300

    menu and cross icon added for Navbar

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,9 @@
 import logo from "./logo.png";
+import menu_icon from "./menu_icon.svg";
+import cross_icon from "./cross_icon.svg";
 
 export const assets = {
     logo,
+    menu_icon,
+    cross_icon,
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,19 +10,51 @@ const Navbar = () => {
           <a href="#Header" className="cursor-pointer hover:text-gray-400">
             Home
           </a>
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a href="#About" className="cursor-pointer hover:text-gray-400">
             About
           </a>
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a href="#Projects" className="cursor-pointer hover:text-gray-400">
             Projects
           </a>
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a
+            href="#Testimonials"
+            className="cursor-pointer hover:text-gray-400"
+          >
             Testimonials
           </a>
         </ul>
         <button className="hidden md:block bg-white py-2 px-8 rounded-full">
           Sign Up
         </button>
+        <img
+          src={assets.menu_icon}
+          alt=""
+          className="md:hidden w-10 cursor-pointer"
+        />
+      </div>
+
+      {/* Mobile Menu */}
+      <div className="md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all">
+        <div className="flex justify-end p-6 cursor-pointer">
+          <img src={assets.cross_icon} alt="" className="md:hidden w-10" />
+        </div>
+        <ul className="flex flex-col items-center gap-2 mt-4 mx-5 text-lg font-medium">
+          <a href="#Header" className="px-4 py-3 rounded-full inline-block">
+            Home
+          </a>
+          <a href="#About" className="px-4 py-3 rounded-full inline-block">
+            About
+          </a>
+          <a href="#Projects" className="px-4 py-3 rounded-full inline-block">
+            Projects
+          </a>
+          <a
+            href="#Testimonials"
+            className="px-4 py-3 rounded-full inline-block"
+          >
+            Testimonials
+          </a>
+        </ul>
       </div>
     </div>
   );
