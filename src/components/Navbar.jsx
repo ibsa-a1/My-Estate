5b9b7ba5861ebef779b594d6e258fdc9b9712567
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-08 00:06:36 +0300

    basic lists of Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,23 @@ const Navbar = () => {
     <div className="absolute top-0 left-0 w-full z-10">
       <div>
         <img src={assets.logo} alt="" />
+        <ul>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Home
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            About
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Projects
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Testimonials
+          </a>
+        </ul>
+        <button className="hidden md:block bg-white py-2 px-8 rounded-full">
+          Sign Up
+        </button>
       </div>
     </div>
   );
