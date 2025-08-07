68c4affe179a3e33ed3e2fb549326bc1fc4c3ea0
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-08 00:58:33 +0300

    Header section included

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { assets } from "./../assets/assets";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <div
+      className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
+      style={{
+        backgroundImage:
+          "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('./src/assets/header_img.jpg')",
+      }}
+      id="Header"
+    >
+      <Navbar />
+      <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
+        <h2 className="text-4xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold">
+          Explore Homes that fits your dreams
+        </h2>
+        <div className="space-x-6 mt-16">
+          <a href="Projects" className="border border-white px-8 py-3 rounded">
+            Projects
+          </a>
+          <a href="Contact" className="bg-blue-500 px-8 py-3 rounded">
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
