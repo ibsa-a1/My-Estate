9cfb917e7c3dfc7c585b685afe184fe2763a1dee
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-09 02:39:43 +0300

    finish about page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,11 @@
 import logo from "./logo.png";
 import menu_icon from "./menu_icon.svg";
 import cross_icon from "./cross_icon.svg";
+import brand_img from "./brand_img.jpg";
 
 export const assets = {
     logo,
     menu_icon,
     cross_icon,
+    brand_img,
 };
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { assets } from "./../assets/assets";
 
 const About = () => {
   return (
@@ -16,6 +17,42 @@ const About = () => {
       <p className="text-gray-500 max-w-80 text-center mb-8">
         Passionate About Properties, Dedicated to Your Vision
       </p>
+      <div className="flex flex-col md:flex-row items-center md:items-start md:gap-20">
+        <img
+          src={assets.brand_img}
+          alt=""
+          className="w-full sm:w-1/2 max-w-md md:w-1/2 lg:w-1/2"
+        />
+        <div className="flex flex-col items-center md:items-start mt-10 text-gray-600">
+          <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
+            <div>
+              <p className="text-4xl font-medium text-gray-800">10+</p>
+              <p>Years of Excellence</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-800">12+</p>
+              <p>Projects Completed</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-800">20+</p>
+              <p>Mn. Sq. Ft. Delivered</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-800">25+</p>
+              <p>Ongoing Projects</p>
+            </div>
+          </div>
+          <p className="my-10 max-w-lg">
+            Discover your perfect home with us. Whether you’re buying, selling,
+            or renting, our friendly experts guide you every step of the way.
+            Explore a wide range of properties matched to your lifestyle and
+            budget. Start your journey to your dream space today!
+          </p>
+          <button className="bg-blue-500 text-white px-8 py-3 rounded cursor-pointer">
+            Learn More
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
