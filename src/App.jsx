424cc0db73fea90b5c724efb483b02cdc1b5d820
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-10 01:34:40 +0300

    Use toast Notification

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
