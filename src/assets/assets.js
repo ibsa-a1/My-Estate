dcff2db8b0999d3ae16c88ac9e04bd932e156c15
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-09 03:35:16 +0300

    add projects data

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,12 @@ import cross_icon from "./cross_icon.svg";
 import brand_img from "./brand_img.jpg";
 import left_arrow from "./left_arrow.svg";
 import right_arrow from "./right_arrow.svg";
+import project_img_1 from "./project_img_1.jpg";
+import project_img_2 from "./project_img_2.jpg";
+import project_img_3 from "./project_img_3.jpg";
+import project_img_4 from "./project_img_4.jpg";
+import project_img_5 from "./project_img_5.jpg";
+import project_img_6 from "./project_img_6.jpg";
 
 export const assets = {
     logo,
@@ -12,4 +18,49 @@ export const assets = {
     brand_img,
     left_arrow,
     right_arrow,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    project_img_5,
+    project_img_6,
 };
+
+export const projectsData = [
+    {
+        title: "Syline Haven",
+        price: "$420,000",
+        location: "Downtown Seattle",
+        image: project_img_1
+    },
+    {
+        title: "Aurora Residences",
+        price: "$385,000",
+        location: "Portland",
+        image: project_img_2
+    },
+    {
+        title: "The Grand Horizon",
+        price: "$510,000",
+        location: "Santa Barbara",
+        image: project_img_3
+    },
+    {
+        title: "Lakeside Serenity",
+        price: "$465,000",
+        location: "Austin",
+        image: project_img_4
+    },
+    {
+        title: "Emerald Heights",
+        price: "$530,000",
+        location: "Lake Tahoe",
+        image: project_img_5
+    },
+    {
+        title: "Sunset Ridge Villas",
+        price: "$399,000",
+        location: "Denver",
+        image: project_img_6
+    },
+];
