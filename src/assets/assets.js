45404dee16124b610f98681e20e1b5940c81765b
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-13 01:30:25 +0300

    use the optimized images

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,20 +1,20 @@
-import logo from "./logo.png";
+import logo from "./logo.webp";
 import menu_icon from "./menu_icon.svg";
 import cross_icon from "./cross_icon.svg";
-import brand_img from "./brand_img.jpg";
+import brand_img from "./brand_img.webp";
 import left_arrow from "./left_arrow.svg";
 import right_arrow from "./right_arrow.svg";
-import project_img_1 from "./project_img_1.jpg";
-import project_img_2 from "./project_img_2.jpg";
-import project_img_3 from "./project_img_3.jpg";
-import project_img_4 from "./project_img_4.jpg";
-import project_img_5 from "./project_img_5.jpg";
-import project_img_6 from "./project_img_6.jpg";
-import profile_img_1 from "./profile_img_1.png";
-import profile_img_2 from "./profile_img_2.png";
-import profile_img_3 from "./profile_img_3.png";
+import project_img_1 from "./project_img_1.webp";
+import project_img_2 from "./project_img_2.webp";
+import project_img_3 from "./project_img_3.webp";
+import project_img_4 from "./project_img_4.webp";
+import project_img_5 from "./project_img_5.webp";
+import project_img_6 from "./project_img_6.webp";
+import profile_img_1 from "./profile_img_1.webp";
+import profile_img_2 from "./profile_img_2.webp";
+import profile_img_3 from "./profile_img_3.webp";
 import star_icon from "./star_icon.svg";
-import header_img from "./header_img.jpg"
+import header_img from "./header_img.webp"
 
 export const assets = {
     logo,
